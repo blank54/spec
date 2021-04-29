cf0e541ede5f318a8fbb40fbc800f1b5b6ba4d1c
SeonghyeonMoon <blank54@snu.ac.kr>
2021-04-29 15:30:56 +0900

    working: different embedding size

--- run/provision_pairing/pairing_by_bert.py
+++ run/provision_pairing/pairing_by_bert.py
@@ -3,20 +3,29 @@
 
 # Configuration
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = str(1) #Do not print INFO
+os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+os.environ['CUDA_VISIBLE_DEVICES'] = str(2)
+
 import sys
+import json
 import codecs
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 
+import keras
+from keras import Input, Model, optimizers
+
 from config import Config
 with open('/data/blank54/workspace/project/spec/spec.cfg', 'r') as f:
     cfg = Config(f)
 
 sys.path.append(cfg['root'])
-from analysis import Read, BERT_Tokenizer
+from analysis import Read, Write, BERT_Tokenizer
 read = Read()
+write = Write()
 
 
 def build_vocab(fpath_vocab):
@@ -39,8 +48,9 @@ def build_vocab(fpath_vocab):
                 continue
     updated_len = len(token_dict)
     
-    print('Initial vocabs: {}'.format(init_len))
-    print('Updated vocabs: {}'.format(updated_len))
+    print('Build BERT Vocabs')
+    print('    | Initial: {}'.format(init_len))
+    print('    | Updated: {}'.format(updated_len))
     reverse_dict = {i: t for t, i in token_dict.items()}
     return token_dict, reverse_dict
 
@@ -53,7 +63,9 @@ def import_data(TARGET_TAG, REFER_TAG):
     fpath = os.path.join(fdir, fname)
 
     data = pd.read_excel(fpath)
-    print('Read data "{}" from "../{}"'.format(fname, cfg['fdir_data_provision_pair_labeled']))
+    print('Load Data for Training')
+    print('    | fdir : ../{}'.format(cfg['fdir_data_provision_pair_labeled']))
+    print('    | fname: {}'.format(fname))
     train, test = train_test_split(data, train_size=0.7, shuffle=True)
     return train, test
 
@@ -80,10 +92,67 @@ def data2input(data, option='train'):
         print('ERROR: Wrong option!!!')
         return None
 
-def load_bert(bert_dist, SEQ_LEN):
+def load_pretrained_model(show_summary):
+    global bert_dist, SEQ_LEN
+
+    # fpath_pretrained_bert = os.path.join(cfg['root'], cfg['fdir_bert_pretrained'], bert_dist)
+    # fpath_bert_config = os.path.join(fpath_pretrained_bert, 'bert_config.json')
+    # with open(fpath_bert_config, 'r', encoding='utf-8') as f:
+    #     config = json.load(f)
+    #     config['vocab_size'] = len(token_dict)
+    #     write.json(obj=config, fpath=fpath_bert_config)
+
     model = read.bert_pretrained(bert_dist=bert_dist, SEQ_LEN=SEQ_LEN)
+
+    if show_summary:
+        model.summary()
+    else:
+        pass
+
     return model
 
+def fine_tuning(model, show_summary):
+    global SEQ_LEN
+
+    inputs = model.inputs[:2]
+    dense = model.layers[-3].output
+    outputs = keras.layers.Dense(
+        units=5,
+        activation='sigmoid',
+        name='ProvisionPairLabel'
+    )(dense)
+
+    bert_model = keras.models.Model(inputs=inputs, outputs=outputs)
+    bert_model.compile(
+        optimizer=keras.optimizers.Adam(learning_rate=0.00001),
+        loss='rmsprop',
+        metrics=['accuracy']
+    )
+
+    # print(bert_model.outputs)
+    if show_summary:
+        bert_model.summary()
+    else:
+        pass
+
+    return bert_model
+
+def train_bert(model):
+    global bert_dist, train_X, train_Y, test_X, test_Y
+    history = model.fit(train_X, train_Y,
+                        epochs=2,
+                        batch_size=16,
+                        verbose=1,
+                        validation_data=(test_X, test_Y),
+                        shuffle=True)
+
+    fname = 'bert.h5'
+    fdir = os.path.join(cfg['root'], cfg['fdir_provision_pairing_bert'])
+    fpath = os.path.join(fdir, fname)
+    model.save_weights(fpath)
+    print('Trained BERT-based ProvisionPairing Model:')
+    print('    | fdir : {}'.format(fdir))
+    print('    | fname: {}'.format(fname))
 
 
 if __name__ == '__main__':
@@ -99,12 +168,13 @@ if __name__ == '__main__':
     fpath_pretrained_bert = os.path.join(cfg['root'], cfg['fdir_bert_pretrained'], bert_dist)
     fpath_vocab = os.path.join(fpath_pretrained_bert, 'vocab.txt')
 
-    # token_dict, reverse_dict = build_vocab(fpath_vocab)
-    # tokenizer = build_tokenizer(token_dict)
+    token_dict, reverse_dict = build_vocab(fpath_vocab)
+    tokenizer = build_tokenizer(token_dict)
 
-    # train, test = import_data(TARGET_TAG, REFER_TAG)
-    # train_X, train_Y = data2input(data=train, option='train')
-    # test_X, test_Y = data2input(test, option='test')
+    train, test = import_data(TARGET_TAG, REFER_TAG)
+    train_X, train_Y = data2input(data=train, option='train')
+    test_X, test_Y = data2input(data=test, option='test')
 
-    model = load_bert(bert_dist=bert_dist, SEQ_LEN=SEQ_LEN)
-    model.summary()
+    model = load_pretrained_model(show_summary=False)
+    bert_model = fine_tuning(model=model, show_summary=False)
+    train_bert(model=bert_model)
